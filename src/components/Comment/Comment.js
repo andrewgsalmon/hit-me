52641565a9e3faa7f9d851854ba3a135d8e8a46e
Andrew Salmon <andrew.g.salmon@gmail.com>
2024-04-15 21:43:28 -0400

    updated styling

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -1,15 +1,29 @@
 import React from 'react'
 import './Comment.scss'
 
-function Comment({artistComment}) {
-  let {comment, name} = artistComment
+function Comment({ artistComment }) {
+  let { comment, name, created_at } = artistComment
+
+  function formatDate(inputDate) {
+    const date = new Date(inputDate);
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = String(date.getFullYear()).slice(-2);
+    return `${day}/${month}/${year}`;
+  }
+
+  const formattedDate = formatDate(created_at);
 
   return (
     <article className='comment'>
       <div className='comment__container'>
         <div className='comment__avatar'></div>
         <div className='comment__info'>
-          <span className='comment__name'>{name}</span>
+          <div className='comment__name-date--desktop'>
+            <span className='comment__name'>{name}</span>
+            <span>{formattedDate}</span>
+          </div>
+          {/* <span className='comment__name comment__name--mobile'>{name}</span> */}
           <p className='comment__text'>{comment}</p>
         </div>
       </div>
