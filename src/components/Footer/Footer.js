f7110ac860e02f828c4521dbaf39b3444794bbd8
Andrew Salmon <andrew.g.salmon@gmail.com>
2024-06-24 14:17:21 -0400

    added social icons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,41 +1,64 @@
-import React, { useEffect, useState } from 'react'
-import { useNavigate, NavLink } from 'react-router-dom';
-import './Footer.scss'
+import React, { useEffect, useState } from "react";
+import { useNavigate, NavLink } from "react-router-dom";
+import "./Footer.scss";
 
 function Footer() {
-  const [token, setToken] = useState(true)
+  const [token, setToken] = useState(true);
   const navigate = useNavigate();
 
   const handleLogout = () => {
     sessionStorage.removeItem("token");
-    setToken(false)
+    setToken(false);
   };
 
   useEffect(() => {
     if (!token) {
-      navigate('/login')
+      navigate("/login");
     }
-  }, [token, navigate])
-
+  }, [token, navigate]);
 
   return (
     <>
-      {token ?
-        <footer className='footer'>
-          <NavLink to={'/home'} className={({ isActive, isPending }) => {
-            return isPending ? "footer__link footer__link--inactive footer__link--home" : isActive ? 'footer__link footer__link--active footer__link--home' : "footer__link footer__link--inactive footer__link--home"
-          }
-          }>Find Music</NavLink>
-          <NavLink to='/profile' className={({ isActive, isPending }) => {
-            return isPending ? "footer__link footer__link--inactive footer__link--profile" : isActive ? 'footer__link footer__link--active footer__link--profile' : "footer__link--inactive footer__link footer__link--profile"
-          }
-          }>Profile</NavLink>
-          <div onClick={handleLogout} className="footer__link footer__link--inactive footer__link--logout">Log Out</div>
+      {token ? (
+        <footer>
+          <nav className="footer">
+            <NavLink
+              to={"/home"}
+              className={({ isActive, isPending }) => {
+                return isPending
+                  ? "footer__link footer__link--inactive footer__link--home"
+                  : isActive
+                  ? "footer__link footer__link--active footer__link--home"
+                  : "footer__link footer__link--inactive footer__link--home";
+              }}
+            >
+              Find Music
+            </NavLink>
+            <NavLink
+              to="/profile"
+              className={({ isActive, isPending }) => {
+                return isPending
+                  ? "footer__link footer__link--inactive footer__link--profile"
+                  : isActive
+                  ? "footer__link footer__link--active footer__link--profile"
+                  : "footer__link--inactive footer__link footer__link--profile";
+              }}
+            >
+              Profile
+            </NavLink>
+            <div
+              onClick={handleLogout}
+              className="footer__link footer__link--inactive footer__link--logout"
+            >
+              Log Out
+            </div>
+          </nav>
         </footer>
-        : ''
-      }
+      ) : (
+        ""
+      )}
     </>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
