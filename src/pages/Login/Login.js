8dd635505d9dbaf9d4c300b453b638db9e10bdaf
Andrew Salmon <andrew.g.salmon@gmail.com>
2024-08-16 15:06:22 -0400

    added frontend UI for oauth functionality

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,16 +1,55 @@
-import React from 'react'
-import Head from '../../layout/Head'
-import './Login.scss'
-import LoginForm from '../../components/LoginForm/LoginForm'
+import React, { useEffect, useState } from "react";
+import { toast, Flip } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Head from "../../layout/Head";
+import "./Login.scss";
+import LoginForm from "../../components/LoginForm/LoginForm";
 
 function Login() {
+  const [signedIn, setSignedIn] = useState(false)
+
+  const notify = () => {
+    toast.success(
+      `Successfully logged in! You will be redirected momentarily...`,
+      {
+        position: "bottom-center",
+        autoClose: 2000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+        transition: Flip,
+      }
+    );
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const token = urlParams.get("token");
+
+    if (token) {
+      sessionStorage.setItem("token", token);
+      setSignedIn(true)
+      notify()
+      setTimeout(() => {
+        window.location.href = "./home";
+      }, 2000);
+    }
+  }, []);
 
   return (
     <>
-      <Head title="Login" description="Login to discover amazing new music with Hit Me!" canonical="/login"/>
-      <LoginForm />
+      <Head
+        title="Login"
+        description="Login to discover amazing new music with Hit Me!"
+        canonical="/login"
+      />
+      <LoginForm signedIn={signedIn} />
+      {/* <ToastContainer /> */}
     </>
-  )
+  );
 }
 
-export default Login
+export default Login;
