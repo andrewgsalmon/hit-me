b27850af49e22da1a59de5be33e56b4bbcef991f
Andrew Salmon <andrew.g.salmon@gmail.com>
2024-09-06 10:21:49 -0400

    updated width of cta section of welcome page

--- src/components/AvatarModal/AvatarModal.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import AvatarModal from './AvatarModal';
-
-test('renders learn react link', () => {
-  render(<AvatarModal />);
-  const linkElement = screen.getByText(/Submit/i);
-  expect(linkElement).toBeInTheDocument();
-});
