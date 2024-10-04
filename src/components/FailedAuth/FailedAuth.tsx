280eebee44378dd3dec413ce733a59eab7eb045b
Andrew Salmon <andrew.g.salmon@gmail.com>
2024-10-04 19:07:09 -0400

    added Giphy API integration into failed auth component

--- src/components/FailedAuth/FailedAuth.tsx
+++ src/components/FailedAuth/FailedAuth.tsx
@@ -1,18 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import garth from '../../assets/images/garth.gif';
-import './FailedAuth.scss';
+import axios from "axios";
+import "./FailedAuth.scss";
+const giphyKey = process.env.REACT_APP_GIPHY_KEY;
 
 function FailedAuth() {
+  const [gif, setGif] = useState("");
+  const [altText, setAltText] = useState("");
 
-  sessionStorage.removeItem('token')
+  sessionStorage.removeItem("token");
+
+  useEffect(() => {
+    const giphy = async () => {
+      try {
+        const response = await axios.get(
+          "https://api.giphy.com/v1/gifs/search",
+          {
+            params: {
+              q: "oops",
+              api_key: giphyKey,
+              rating: "pg",
+            },
+          }
+        );
+
+        const random = Math.floor(Math.random() * 25);
+
+        setGif(response.data.data[random].images.original.url);
+        setAltText(response.data.data[random].title);
+      } catch (error) {
+        console.error(`hmm... API call didn't go through! ${error}`);
+      }
+    };
+
+    giphy();
+  }, []);
 
   return (
     <section className="failed-auth">
+      <span className="failed-auth__giphy-attribution">randomly powered by Giphy</span>
       <img
         className="failed-auth__giphy-embed"
-        src={garth}
-        alt="gif of garth from waynes world saying no way"
+        src={gif}
+        alt={`${altText} - powered by Giphy`}
       />
       <p>
         WHOOPS! <br />
